51aa7a0ffab7c5d97a941c0eeae3df9f60e0d92d
Asjah Marie <117318989+AsjahMarie@users.noreply.github.com>
2023-10-27 20:37:24 -0500

    Trying to make it somewhat match the wireframe sent

--- APP/script.js
+++ APP/script.js
@@ -1,58 +1,35 @@
-const addTaskBtn = document.getElementById('add-task-btn');
-const taskInput = document.getElementById('task-input');
-const taskList = document.getElementById('task-list');
-const username = document.getElementById('username').value;
-const password = document.getElementById('password').value;
-
-// Task model
-class Task {
-    constructor(taskText) {
-        this.taskText = taskText;
-    }
+// Function to validate the password format
+function validatePassword(password) {
+    // Password regex pattern: At least 10 characters, 1 special character, and 1 number
+    const passwordPattern = /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d).{10,}$/;
+    return passwordPattern.test(password);
 }
 
-// Task view
-function displayTask(task) {
-    const taskDiv = document.createElement('div');
-    taskDiv.classList.add('task');
+// Function to handle form submission
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-    const taskText = document.createElement('p');
-    taskText.textContent = task.taskText;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.addEventListener('click', () => {
-        taskList.removeChild(taskDiv);
-    });
-
-    taskDiv.appendChild(taskText);
-    taskDiv.appendChild(deleteBtn);
-
-    taskList.appendChild(taskDiv);
-}
-
-// Task controller
-function addTask() {
-    const taskText = taskInput.value.trim();
-    if (taskText) {
-        const task = new Task(taskText);
-        displayTask(task);
-        taskInput.value = '';
-    }
-}
-
-addTaskBtn.addEventListener('click', addTask);
-
-document.getElementById('login-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    // Replace the below condition with your actual login condition
-    if (username === 'test' && password === 'test') {
-        window.location.href = 'home.html';
+    if (usernameTaken(username)) {
+        showError('Username is already taken');
+    } else if (!validatePassword(password)) {
+        showError('Password must be at least 10 characters with at least 1 special character and 1 number');
     } else {
-        alert('Invalid credentials');
+        // Register the user (proceed to the registration process)
+        registerUser(username, password);
     }
 });
 
+// Function to show error messages
+function showError(message) {
+    document.getElementById('error-msg').innerText = message;
+}
 
+// Simulated function to register the user
+function registerUser(username, password) {
+    // Replace this with your actual registration process
+    console.log('User registered:', username, password);
+    // This is where you would proceed to the actual registration process (e.g., send data to server)
+}
