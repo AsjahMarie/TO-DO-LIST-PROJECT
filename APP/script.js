f2d8ffd46672091a4df552d3004955fca2e96eb0
Asjah Marie <117318989+AsjahMarie@users.noreply.github.com>
2023-10-29 01:31:59 -0500

    Made a Interactive/Animated login page.

--- APP/script.js
+++ APP/script.js
@@ -1,67 +1,14 @@
-// Function to validate the password format
-function validatePassword(password) {
-    // Password regex pattern: At least 10 characters, 1 special character, and 1 number
-    const passwordPattern = /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d).{10,}$/;
-    return passwordPattern.test(password);
-}
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('register')
 
-// Function to handle form submission
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+// Register button event listener
+registerBtn.addEventListener('click', () => {
 
-    const Username = document.getElementById('Username').value;
-    const Password = document.getElementById('Password').value;
-
-    if (usernameTaken(username)) {
-        showError('Username is already taken');
-    } else if (!validatePassword(password)) {
-        showError('Password must be at least 10 characters with at least 1 special character and 1 number');
-    } else {
-        // Register the user (proceed to the registration process)
-        registerUser(username, password);
-    }
+    container.classList.add("active");
 });
-
-// Function to show error messages
-function showError(message) {
-    document.getElementById('error-msg').innerText = message;
-}
-// A sample list of registered users
-let registeredUsers = [
-    {username: 'user1', password: 'password1'},
-    {username: 'user2', password: 'password2'},
-];
-
-// A sample registration function
-function registerUser(username, password) {
-    // Check if the username is already registered
-    for (let user of registeredUsers) {
-        if (user.username === username) {
-            console.log('Username already exists.');
-            return;
-        }
-    }
-
-    // If the username is not already registered, add it to the list of registered users
-    registeredUsers.push({username: username, password: password});
-    console.log('User successfully registered.');
-}
-// Simulated function to register the user
-function registerUser(Username, Password) {
-    // Replace this with your actual registration process
-    console.log('User registered:', Username, Password);
-    // This is where you would proceed to the actual registration process (e.g., send data to server)
-}
-
-// Function to handle successful login
-function handleSuccessfulLogin() {
-    // Redirect to the homepage after successful login
-    window.location.href = 'HomePage.html'; 
-}
-
-// Function to handle successful signup
-function handleSuccessfulSignup() {
-    // Redirect to the homepage after successful signup
-    window.location.href = 'HomePage.html'; 
-}
+    
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
 
